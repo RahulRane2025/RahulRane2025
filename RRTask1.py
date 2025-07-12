90d4bbbc48795be4de7b8a195f3fe376ac1a93cc
RahulRane2025 <notmebutyoupurpose2021@gmail.com>
2025-07-12 14:33:38 +0530

    Add files via upload

---
+++ RRTask1.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter the First Number:"))
+n2=int(input("Enter the Second Number:"))
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1/n2
+print("addition:", add)
+print("Substraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
---
+++ RRTask2.py
@@ -0,0 +1,3 @@
+Fname=input("Enter the First Name:")
+Lname=input("Enter the Last Name:")
+print(f"Hello {Fname} {Lname} !Welcome to the Python Program")
